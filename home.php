a88521341cb6777394fe421f183918e5204caaa5
Baher Talal <baher@ivas.com.eg>
2019-10-08 17:13:20 +0200

    make template

---
+++ home.php
@@ -0,0 +1,136 @@
+<!-- Start Header -->
+<?php include 'header.php';?>
+<!-- End Header-->
+<section class="menu-container">
+    <div class="menu-wrapper">
+        <div class="categories-wrapper">
+            <ul class="menu-categories list-unstyled text-right">
+                <div class="row">
+                    <!-- menu link 1 -->
+                    <li>
+                        <a class="li_link" href="#">
+                            <div class="col-3">
+                                <i class="fa fa-home"></i>
+                            </div>
+
+                            <div class="col-9">
+                                <div>الرئيسية</div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <!-- menu link 2 -->
+                    <li>
+                        <a class="li_link" href="#">
+                            <div class="col-3">
+                                <i class="fa fa-music"></i>
+                            </div>
+
+                            <div class="col-9">
+                                <div>أدعية الانتظار</div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <!-- menu link 3 -->
+                    <li>
+                        <a class="li_link" href="#">
+                            <div class="col-3">
+                                <i class="fa fa-film"></i>
+                            </div>
+
+                            <div class="col-9">
+                                <div>فيديو</div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <!-- menu link 4 -->
+                    <li>
+                        <a class="li_link" href="#">
+                            <div class="col-3">
+                                <i class="fa fa-headphones"></i>
+                            </div>
+
+                            <div class="col-9">
+                                <div>صوتيات</div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <!-- menu link 5 -->
+                    <li>
+                        <a class="li_link" href="#">
+                            <div class="col-3">
+                                <i class="fa fa-camera"></i>
+                            </div>
+
+                            <div class="col-9">
+                                <div>بطاقة التهنئة</div>
+                            </div>
+                        </a>
+                    </li>
+
+                    <!-- menu link 6 -->
+                    <li>
+                        <a class="li_link" href="#">
+                            <div class="col-3">
+                                <i class="fa fa-info-circle"></i>
+                            </div>
+
+                            <div class="col-9">
+                                <div>الارشادات</div>
+                            </div>
+                        </a>
+                    </li>
+                </div>
+            </ul>
+        </div>
+
+        <div class="social-media-wrapper">
+            <div class="copyright">Powered By </div>
+            <div>
+                <img class="ooredoo-logo-img ivas" src='img/ivas.png' />
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="site-wrapper">
+    <header>
+        <nav class="main-nav">
+            <div class="main-nav-wrapper">
+                <ul class="status-toolbar list-unstyled">
+                    <li class="welcome text-center">
+                        <p>السلام عليكم ورحمة الله وبركاته</p>
+                    </li>
+                </ul>
+
+                <div class="container">
+                    <div class="row">
+                        <ul class="menu-toolbar make-sticky cf list-unstyled">
+                            <li class="col-2 burger-menu-btn">
+                                <i class="fa fa-bars fa-2x"></i>
+                            </li>
+
+                            <li class="col-8 service-logo cf">
+                                <div class="logo-text">
+                                    <img src="img/logotext_grey.svg" alt="">
+                                </div>
+                            </li>
+
+                            <li class="col-2 logo-img RotateLogo2">
+                                <a href='#'>
+                                    <img src='img/ooredoologo.png' alt="" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</div>
+<!-- Start Footer -->
+<?php include 'footer.php';?>
+<!-- End Footer-->
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- metas -->
+    <meta charset="utf-8">
+    <!-- IE compatibility meta -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- for phons -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- links -->
+    <link rel="stylesheet" href='css/bootstrap.min.css'>
+    <link rel="stylesheet" href='css/all.min.css'>
+    <link rel="stylesheet" href='css/style.css'>
+    <!-- title -->
+    <title>Template</title>
+</head>
+<style>
+body {
+    background-image: linear-gradient(rgba(255, 255, 255, 0), #4c4a4a);
+}
+</style>
+
+<body>
+    <div class="welcome_page">
+        <div class="logo">
+            <img class="rounded" src="img/mobinil-logo.png" alt="Test">
+        </div>
+        <a href="home.php" class="wow pulse" data-wow-delay="300ms" data-wow-iteration="infinite"
+            data-wow-duration="1.5s">
+            <i class="fas fa-angle-double-right fa-2x"></i>
+        </a>
+        <img src="img/ivas.png" alt="ivas">
+    </div>
